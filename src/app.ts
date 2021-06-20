99aa150c2f1db0822de47e9c10cfd71be637537a
soeun727 <yeajin0727@gmail.com>
2021-06-20 12:56:11 +0900

    2021.6.20

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import { createServer, Server } from 'http';
 
+import controller from './controller';
+
 const app = express();
+
+app.use(express.json());
+app.use(controller);
+
 const server = createServer(app);
 server.listen(process.env.PORT || 5000);
